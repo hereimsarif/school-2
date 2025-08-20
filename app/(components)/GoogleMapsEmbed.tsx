e72e03ee9bb99e17230874ef4d8b67d104a61444
hereimsarif <131228155+hereimsarif@users.noreply.github.com>
2025-08-21 00:54:23 +0530

    chore: add app/(components)/GoogleMapsEmbed.tsx

---
+++ app/(components)/GoogleMapsEmbed.tsx
@@ -0,0 +1,7 @@
+export default function GoogleMapsEmbed() {
+  return (
+    <div className="w-full aspect-video rounded-xl overflow-hidden bg-gray-200 dark:bg-gray-800 flex items-center justify-center">
+      <span className="text-gray-500 text-lg">[Google Maps Embed Here]</span>
+    </div>
+  );
+}
